2ca818d267b59aa1f601ff075e31159367328e20
nikhil-njy <nikhil.njy@gmail.com>
2022-02-24 16:41:41 +0530

    first commit

---
+++ src/main/java/com/Feb23Demo1/Feb23Demo1Application.java
@@ -0,0 +1,13 @@
+package com.Feb23Demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Feb23Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Feb23Demo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Feb23Demo1/Feb23Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Feb23Demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Feb23Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
